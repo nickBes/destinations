c1ff8a1fcb225150ae08629e2f35c701217dcf2c
nickBes <>
2021-11-19 18:58:49 +0200

    now have fully functioning and aligned cards

--- scripts/index.js
+++ scripts/index.js
@@ -5,9 +5,17 @@ const dataMap = {
     "Address": "מקום",
     // "Phone":"טלפון",
     // "Email":"מייל",
-    "Product_Url":"אתר",
+    "URL":"אתר",
     "Opening_Hours":"שעות פתיחה"
 }
+
+const headerKey = "Name"
+const filterKey = "Attraction_Type"
+const urlKey = "URL"
+
+// creates an array of css bg-color attribute that is mapped to the variables
+// that exist in index.css
+const headerColors = "abc".split("").map(value => `background-color: var(--${value})`)
 
 async function getDestinationData () {
     const url = "https://data.gov.il/api/3/action/datastore_search"
@@ -60,7 +68,7 @@ async function calculateMatchingScore (baseString, searchString) {
 }
 
 
-async function filterSuggetions (event, records, types, dataKey) {
+async function filterSuggetions (event, records, types) {
     // prevents from running default method during this event
     event.preventDefault()
 
@@ -75,21 +83,20 @@ async function filterSuggetions (event, records, types, dataKey) {
     if (!filter || (!types.has(filter) && filter != "default")) return
 
     suggetions.removeChildren()
-    let filtered = filter == "default" ? records : records.filter(value => value["Attraction_Type"] == filter)
+    let filtered = filter == "default" ? records : records.filter(value => value[filterKey] == filter)
   
     if (text != "") {
         // create a map between each record's matching score and their indexes,
         // where the matching score is the sum of the shared strings lengths
         let map = await Promise.all(filtered.map(async (record, index) => {
             return {
-                matchingScore: await calculateMatchingScore(record[dataKey], text),
+                matchingScore: await calculateMatchingScore(record[headerKey], text),
                 index: index
             }
         }))
-        map.forEach(val => console.log(records[val.index], val.matchingScore))
         // filters by the filter value from the selections and by the score
         // also sorts the results
-        map = map.filter(async value => value.matchingScore > 0)
+        map = map.filter(value => value.matchingScore > 0)
                 .sort((leftValue, rightValue) => {
                     // will switch places if rightValue is bigger the leftValue
                     return rightValue.matchingScore - leftValue.matchingScore
@@ -103,20 +110,48 @@ async function filterSuggetions (event, records, types, dataKey) {
             suggetions.appendChild(sug)
         })
     } else {
-        await filtered.forEach(value => {
-            let objectData = filterRecordDataFromMap(value, dataMap)     
+        filtered.forEach(value => {
+            let objectData = filterRecordDataFromMap(value)     
             let sug = createSuggestionComponent(objectData)
             suggetions.appendChild(sug)
         })
     }
+    // the situation where there are no results
+    let thereAreResults = true
+    if (suggetions.children.length == 0) {
+        let noResult = new Component("h1")
+        noResult.addAttributes({id: "no-rslt"})
+        noResult.text = "אין תוצאות רלוונטיות"
+        suggetions.appendChild(noResult)
+        thereAreResults = false
+    }
     suggetions.renderChildren()
+
+    // will add the expand function to children that their text is overflowing
+    if (thereAreResults) {
+        suggetions.children.forEach(child => {
+            createExpandMethod(child.htmlElement)
+        })
+    }
 }
 
-function filterRecordDataFromMap(record, map) {
+function createExpandMethod(element) {
+    const isOverflowing = element.clientHeight < element.scrollHeight
+    if (isOverflowing) {
+        element.onclick = () => {
+            element.classList.toggle("card-ratio")
+            element.classList.toggle("card-more")
+        }
+        element.classList.add("card-more")
+        element.setAttribute("title", "קרא עוד")
+    }
+}
+
+function filterRecordDataFromMap(record) {
     let n = {}
     for (const key in record) {
-        if (key in map) {
-            n[map[key]] = record[key]
+        if (key in dataMap) {
+            n[dataMap[key]] = record[key]
         }
     }
     return n
@@ -124,14 +159,38 @@ function filterRecordDataFromMap(record, map) {
 
 function createSuggestionComponent(dataObject) {
     let sug = new Component("li")
-    let data = new Component("ul")
+    sug.htmlElement.classList.add("card", "card-ratio")
+
+    let mappedHeaderKey = dataMap[headerKey]
+    let mappedUrlKey = dataMap[urlKey]
+
+    // creates a heading for the card
+    if (mappedHeaderKey && mappedHeaderKey in dataObject) {
+        let header = new Component("h3")
+        header.htmlElement.classList.add("card-header")
+        header.text = dataObject[mappedHeaderKey]
+        header.addAttributes({style: headerColors[Math.round(Math.random() * (headerColors.length - 1))]})
+        sug.appendChild(header)
+
+        delete dataObject[mappedHeaderKey]
+    }
+
+    // create a url for the card
+    if (mappedUrlKey && mappedUrlKey in dataObject) {
+        let url = new Component("a")
+        url.htmlElement.classList.add("card-url")
+        url.addAttributes({href: dataObject[mappedUrlKey]})
+        url.text = "עבור לאתר"
+        sug.appendChild(url)
+        
+        delete dataObject[mappedUrlKey]
+    }
 
     for (const key in dataObject) {
-        let d = new Component("li")
+        let d = new Component("p")
         d.text = `${key}: ${dataObject[key]}`
-        data.appendChild(d)
+        sug.appendChild(d)
     }
-    sug.appendChild(data)
     return sug
 }
 
@@ -145,7 +204,7 @@ async function start () {
         const types = new Set()
         const selection = new Component("#selection")
         for (const record of records) {
-            types.add(record["Attraction_Type"])
+            types.add(record[filterKey])
         }
         for (const type of types.values()) {
             let option = new Component("option")
@@ -155,7 +214,7 @@ async function start () {
         }
         selection.renderChildren()
         const form = new Component("#frm")
-        form.htmlElement.onsubmit = (event) => filterSuggetions(event, records, types, "Name")
+        form.htmlElement.onsubmit = (event) => filterSuggetions(event, records, types)
     } else {
         const root = new Component("#root")
         root.delete()
